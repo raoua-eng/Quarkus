3b26cd16003d64f0500064fa156c55a0df5b99ff
raoua <raoua.khaterchi@gmail.com>
2022-06-07 09:37:09 +0100

    WIP

--- src/main/java/org/acme/graphql/mutations/PersonGraphQLResource.java
+++ src/main/java/org/acme/graphql/mutations/PersonGraphQLResource.java
@@ -21,7 +21,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @GraphQLApi
 //@LocaleChangeInterceptor
-//@Authenticated
+@Authenticated
 public class PersonGraphQLResource{
     @Inject
     ObjectMapper objectMapper;
